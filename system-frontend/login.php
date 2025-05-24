1f93e0960f6f6b11e02565fbd72df3bfd87b93bb
Laxsnorx <rp.oreiro@mlgcl.edu.ph>
2025-05-24 21:10:11 +0800

    system

--- system-frontend/login.php
+++ system-frontend/login.php
@@ -178,7 +178,7 @@
       const password = document.getElementById("password").value;
 
       try {
-        const response = await fetch("http://127.0.0.1:8000/api/login", {
+        const response = await fetch("https://rgradebackend.bdedal.online/api/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
